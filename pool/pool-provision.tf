decb493bb0bdcf195f97224a76df792bf2f21180
Katleho Lephallo <katleho@pop-os.lan>
2026-05-06 19:13:35 +0200

    making sure the pool is started when code is run

--- pool/pool-provision.tf
+++ pool/pool-provision.tf
@@ -2,8 +2,29 @@
 resource "null_resource" "create_vm-images" {
   provisioner "remote-exec" {
     inline = [
-      "mkdir -p /home/dexter-1/vm-images"
+      "mkdir -p /home/dexter-1/vm-images",
     ]
+  }
+
+
+    # Connects with ssh to create the directory
+  connection {
+    type        = "ssh"
+    user        = "dexter-1"
+    host        = "master"
+    private_key = file("~/.ssh/id_ed25519")
+  }
+}
+
+  # It starts the pool and makes it autostart if it is not the case
+resource "null_resource" "start_vm_pool" {
+
+  provisioner "remote-exec" {
+    inline = [
+      "virsh pool-start vm-pool || true",
+      "virsh pool-autostart vm-pool"
+    ]
+    
   }
 
     # Connects with ssh to create the directory
@@ -13,4 +34,7 @@ resource "null_resource" "create_vm-images" {
     host        = "master"
     private_key = file("~/.ssh/id_ed25519")
   }
+
+  depends_on = [libvirt_pool.vm_pool]
+
 }
